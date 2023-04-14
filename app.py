ffdaf0182c67ca154440a727e3f6eeeb31505030
Aayush Seth <aayushseth8@gmail.com>
2023-04-14 17:59:54 -0400

    Create basic hesitation, clarity, and pause detection

--- app.py
+++ app.py
@@ -1,46 +1,70 @@
 from flask import Flask, render_template, request, redirect
 
-import speech_recognition as sr
 import sounddevice as sd
-from scipy.io.wavfile import write
-import numpy as np
+
 import wavio
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import whisper
 
 
 app = Flask(__name__)
 
-authenticator = IAMAuthenticator('44JTbkXwxOgiIb9S-Q9PKSoz_kY8Su-ZCxuDpH5kt1Sx')
-
 def recordSound():
 
-
     fs = 44100  # Sample rate
-    seconds = 3  # Duration of recording
+    seconds = 10  # Duration of recording
 
+    print('start recording')
     myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
     sd.wait()  # Wait until recording is finished
+    print('end recording')
 
     wavio.write("output.wav", myrecording, fs, sampwidth=3) # Save as WAV file 
 
-    recognizer = sr.Recognizer()
-    audioFile = sr.AudioFile('./output.wav')
-    with audioFile as source:
-        data = recognizer.record(source)
-    transcript = recognizer.recognize_google(data, key=None)
-    # transcript = ""
-    # try:
-    # transcript = recognizer.recognize_ibm(data, key="44JTbkXwxOgiIb9S-Q9PKSoz_kY8Su-ZCxuDpH5kt1Sx")
-    # transcript = recognizer.recognize_ibm(data, username="apikey", password="44JTbkXwxOgiIb9S-Q9PKSoz_kY8Su-ZCxuDpH5kt1Sx")
-    # except:
-    #     print("Error with transcription try again")
+    transcript = "no text"
+    model = whisper.load_model("base")
+    result = model.transcribe("output.wav", fp16=False, word_timestamps=True)
 
-    return transcript
+    ideal_seconds_per_word = 60 / 115 # 115 wpm
 
+    unclear_words = []
+    hesitation_words = []
+    pause_timestamps = []
+    
+    last_word = None
+    for segment in result["segments"]:
+        for cur_word in segment['words']:
+            print(cur_word)
+            if cur_word['probability'] < 0.7:
+                unclear_words.append((cur_word['word'], cur_word['probability']))
+            if cur_word['end'] - cur_word['start'] > ideal_seconds_per_word:
+                hesitation_words.append((cur_word['word'], cur_word['end'] - cur_word['start']))
+            
+            if last_word and cur_word['start'] - last_word['end'] > 0:
+                pause_timestamps.append((last_word['end'], cur_word['start']))
+        
+        last_word = cur_word
+    print("Unclear Words:", unclear_words)
+    print("Hesitation Words:", hesitation_words)
+    print("Pause Timestamps:", pause_timestamps)
+
+
+    unclear = str(unclear_words).strip('[]')
+
+    hesitation = str(hesitation_words).strip('[]')
+
+    pause =  str(pause_timestamps).strip('[]')
+
+    transcript = result["text"]
+    
+
+    return transcript, unclear, hesitation, pause
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     transcript = ""
+    unclear = ""
+    hesitation = ""
+    pause = ""
     if request.method == "POST":
         if "transcribe" in request.form:
             print("Form Data Received")
@@ -53,18 +77,11 @@ def index():
                 return redirect(request.url)
             
             if file:
-                recognizer = sr.Recognizer()
-                audioFile = sr.AudioFile(file)
-                with audioFile as source:
-                    data = recognizer.record(source)
-                transcript = recognizer.recognize_google(data, key=None)
-                print(transcript)
+                pass
         elif "record" in request.form:
-            transcript = recordSound()
+            transcript, unclear, hesitation, pause = recordSound()
 
-
-
-    return render_template('index.html', transcript=transcript)
+    return render_template('index.html', transcript=transcript, unclear=unclear, hesitation=hesitation, pause=pause)
 
 if __name__=="__main__":
     app.run(debug=True, threaded=True)
